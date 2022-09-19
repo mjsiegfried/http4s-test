f29bcef4ccd55c4cc522935b54424c40ab9ff87d
Max Siegfried <mjsiegfried@gmail.com>
2022-09-19 13:25:55 -0700

    doobie wip

--- build.sbt
+++ build.sbt
@@ -3,6 +3,7 @@ ThisBuild / version := "0.1.0-SNAPSHOT"
 ThisBuild / scalaVersion := "2.13.8"
 
 val http4sVersion = "0.23.16"
+lazy val doobieVersion = "1.0.0-RC1"
 
 lazy val root = (project in file("."))
   .settings(
@@ -11,5 +12,11 @@ lazy val root = (project in file("."))
       "org.http4s" %% "http4s-dsl" % http4sVersion,
       "org.http4s" %% "http4s-ember-server" % http4sVersion,
       "org.http4s" %% "http4s-ember-client" % http4sVersion,
-      "org.typelevel" %% "cats-effect" % "3.3.12")
+      "org.typelevel" %% "cats-effect" % "3.3.14",
+      "ch.qos.logback" % "logback-classic" % "1.4.0",
+      "org.typelevel" %% "log4cats-slf4j"   % "2.5.0",
+      "org.tpolecat" %% "doobie-core" % doobieVersion,
+      "org.tpolecat" %% "doobie-postgres" % doobieVersion,
+      "org.tpolecat" %% "doobie-specs2" % doobieVersion
+    ),
   )
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,21 +1,43 @@
 import cats.effect._
+import cats.implicits.catsSyntaxApplicativeId
 import com.comcast.ip4s.IpLiteralSyntax
+import doobie.implicits._
+import doobie.{ConnectionIO, Transactor}
 import org.http4s._
 import org.http4s.client.Client
 import org.http4s.dsl.io._
 import org.http4s.ember.client.EmberClientBuilder
 import org.http4s.ember.server.EmberServerBuilder
 import org.http4s.implicits.http4sLiteralsSyntax
+import org.typelevel.log4cats.slf4j.Slf4jLogger
 
 object Main extends IOApp {
 
+  val logger = Slf4jLogger.getLogger[IO]
 
   def helloWorldService(client: Client[IO]) = HttpRoutes.of[IO] {
     case GET -> Root =>
       val target = uri"http://localhost:8080/hello/" / "Bob"
-      Ok(client.expect[String](target))
+      val result = client.expect[String](target)
+      Ok(result)
+    case GET -> Root / "db" =>
+      val program1 = "42".pure[ConnectionIO]
+      val xa = Transactor.fromDriverManager[IO](
+        "org.postgresql.Driver", // driver classname
+        "jdbc:postgresql:world", // connect URL (driver-specific)
+        "postgres", // user
+        "" // password
+      )
+      val result = program1
+        .transact(xa)
+        .handleErrorWith(err => logger.error(err.toString)
+          .map(_ => "Err"))
+      Ok(result)
+
     case GET -> Root / "hello" / name =>
       Ok(s"Hello, $name.")
+
+
   }.orNotFound
 
   def run(args: List[String]): IO[ExitCode] =
