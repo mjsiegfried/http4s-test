6d7ee46ee49d9b5739a1f8abf9879e68114051f7
Max Siegfried <mjsiegfried@gmail.com>
2022-09-20 14:35:28 -0700

    wrote generic serde test

--- src/test/scala/SerdeTest.scala
+++ src/test/scala/SerdeTest.scala
@@ -1,24 +1,30 @@
 import DatabaseContract.Country
+import SerdeTest.testCountry
 import TestModel.{Test, TestError, TestFailed, TestPass, TestResult, TestSuite}
 import cats.data.EitherT
 import cats.effect.IO
 import cats.implicits._
-import io.circe.{Json, parser}
+import io.circe.{Decoder, Encoder, Json, parser}
 import io.circe.syntax.EncoderOps
 
 object SerdeTest extends Serde {
 
-  val testCountry = Country("TEST", "Test Land", 4, Some(10.5))
-  val x = parser.parse(testCountry.asJson.noSpaces).getOrElse(Json.Null).as[Country] match {
-    case Left(value) => TestFailed(value.getMessage())
-    case Right(value) => if(value == testCountry) TestPass() else TestFailed(s"Objects did not match $value : $testCountry")
+  def testParsingSymmetry[T](obj: T)(implicit e: Encoder[T], d: Decoder[T]) = {
+    parser.parse(obj.asJson.noSpaces).getOrElse(Json.Null).as[T] match {
+      case Left(value) => TestFailed(value.getMessage())
+      case Right(value) => if (value == obj) TestPass() else TestFailed(s"Parsed object: $value did not match original object: $obj ")
+    }
   }
 
   val test1: EitherT[IO, TestError, Test] = EitherT(Test("test1", TestPass()).asRight[TestError].pure[IO])
 
+  val testCountry = Country("TEST", "Test Land", 4, Some(10.5))
+  val test2 = EitherT(Test("Test Country Serde is symmetrical", testParsingSymmetry(testCountry) ).asRight[TestError].pure[IO])
+
   def tests(): EitherT[IO, TestError, TestSuite] = {
     List(
-      test1
+      test1,
+      test2
     ).sequence.map(tests => TestSuite("SerdeTest", tests))
 
   }
