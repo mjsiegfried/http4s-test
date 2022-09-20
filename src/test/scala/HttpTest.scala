f487938ae11d106c379b2b57ecc7d1cc5154b593
Max Siegfried <mjsiegfried@gmail.com>
2022-09-20 16:12:10 -0700

    added http test

--- src/test/scala/HttpTest.scala
+++ src/test/scala/HttpTest.scala
@@ -1,21 +1,32 @@
-import TestModel.{Test, TestError, TestSuite}
+import TestModel.{Test, TestError, TestFailed, TestPass, TestResult, TestSuite}
 import cats.data.EitherT
 import cats.effect.IO
-import cats.implicits.toTraverseOps
+import cats.implicits.{catsSyntaxEitherId, toTraverseOps}
 import org.http4s.client.Client
 import org.http4s.implicits.http4sLiteralsSyntax
 
 object HttpTest {
 
-//  def test1(implicit client: Client[IO]): EitherT[IO, TestError, Test] = {
-//    val target = uri"http://localhost:8080/hello/" / "Bob"
-//    val result = client.expect[String](target)
-//    result
-//  }
+  def testHelloEndpoint(implicit client: Client[IO]): EitherT[IO, TestError, Test] = {
+    val target = uri"http://localhost:8080/hello/" / "Bob"
+    val result = client.expect[String](target)
+    val expectedValue = "Hello, Bob."
+
+    EitherT(result.attempt.map {
+      case Left(value) => TestFailed(s"Http Error: $value")
+      case Right(value) => if (value == expectedValue) {
+        TestPass()
+      }
+      else {
+        TestFailed(s"Returned value: $value did not match expected value: $expectedValue")
+      }
+    }.map(result => Test("Test Hello Endpoint", result).asRight[TestError]))
+
+  }
 
   def tests(implicit client: Client[IO]): EitherT[IO, TestError, TestSuite] = {
     List(
-     SerdeTest.testCountryIsSymmetrical
+      testHelloEndpoint
     ).sequence.map(tests => TestSuite("HttpTest", tests))
 
   }
