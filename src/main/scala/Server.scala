0e88937e4ab3f912287ddac1eedb23301b7ebbea
Max Siegfried <mjsiegfried@gmail.com>
2022-09-21 09:39:48 -0700

    cleaned up a bit

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -16,24 +16,6 @@ import org.typelevel.ci.CIStringSyntax
 object Server extends Serde {
 
   def tubiService(client: Client[IO])(implicit databaseContract: DatabaseContract, logger: Logger[IO]): Kleisli[IO, Request[IO], Response[IO]] = HttpRoutes.of[IO] {
-    case GET -> Root =>
-      val target = uri"http://localhost:8080/hello/" / "Bob"
-      val result = client.expect[String](target)
-      Ok(result)
-    case GET -> Root / "getFirst" =>
-      databaseContract.getCountry
-        .map(country => Ok(country.asJson.noSpaces))
-        .leftMap {
-          case DatabaseError.ObjectNotFoundError(msg) =>
-            Ok(s"Object not found $msg")
-          case DatabaseError.UnknownDatabaseError(msg) =>
-            InternalServerError(msg)
-
-        }.value.flatMap(_.merge)
-    case GET -> Root / "hello" / name =>
-      Ok(s"Hello, $name.")
-
-    // todo -- new stuff below
     case GET -> Root / "tubi" =>
       val request = GET(
         uri"http://mock-content.interview.staging.sandbox.tubi.io/api/content/all?size=100&type=movie",
@@ -41,15 +23,14 @@ object Server extends Serde {
         Accept(MediaType.application.json)
       )
 
-      val result = client.expect[String](request).map { response =>
+      val result = client.expect[String](request).flatMap { response =>
         parser.parse(response).getOrElse(Json.Null).as[ContentResponse] match {
-          case Left(value) => s"Failed to parse response into class: $response Error: $value"
-          case Right(value) => value.asJson.noSpaces
+          case Left(value) => InternalServerError(s"Failed to parse response into class: $response Error: $value")
+          case Right(value) => Ok(value.items.asJson.noSpaces)
         }
-
-
       }
 
-      Ok(result)
+      result
+
   }.orNotFound
 }
